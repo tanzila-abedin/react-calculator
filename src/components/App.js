0e5432520f42ee17d0adafdc0a4bc0b22f7582c3
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-11 22:29:46 +0200

    Create app as a functional component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>App components</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
