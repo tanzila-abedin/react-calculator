5344663088f934fe84d3672e7b383ab10cc9cde1
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-18 13:54:47 +0200

    Import css file  on components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-// import '../App.css';
+import '../App.css';
 import PropTypes from 'prop-types';
 
 const Button = ({ className, value, onClick }) => (
--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,6 @@
 // import PropTypes from 'prop-types';
 import PropTypes from 'prop-types';
-// import '../App.css';
+import '../App.css';
 
 const ButtonPanel = ({ children }) => (
   <div className="buttonPanel">{children}</div>
