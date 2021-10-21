dd6690e0c38580e821cf6cf9e9de9e45aea4aad8
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-22 00:15:08 +0200

    Create mock data to display random quotes from it

---
+++ src/data/db.js
@@ -0,0 +1,29 @@
+const mockData = [
+  {
+    images: '../assets/Blog-Caltech-writers-Bell.jpg',
+    body: 'Obvious is the most dangerous word in mathematics.',
+    author: 'E. T. Bell',
+    id: 1,
+  },
+  {
+    images: '../assets/lbers.jpeg',
+    body: 'Mathematics is a collection of cheap tricks and dirty jokes.',
+    author: 'Lipman Bers',
+    id: 2,
+  },
+  {
+    images: '../assets/Henri_de_Blainville.jpg',
+    body: 'Life is the twofold internal movement of composition and decomposition at once general and continuous.',
+    author:
+      'Henri de Blainville (American Journal of the Medical Sciences, 1858)',
+    id: 3,
+  },
+  {
+    images: '../assets/Bernard_Bolzano.jpg',
+    body: 'My special pleasure in mathematics rested particularly on its purely speculative part.',
+    author: 'Bernhard Bolzano (1781-1848)',
+    id: 4,
+  },
+];
+
+export default mockData;
