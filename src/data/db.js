abd8bbf5cc1076fdc2b30f16245b0dfb0e4ba846
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-22 12:47:51 +0200

    Import images to be called inside mockdata image property

--- src/data/db.js
+++ src/data/db.js
@@ -1,27 +1,31 @@
+import Bell from '../assets/Blog-Caltech-writers-Bell.jpg';
+import Bers from '../assets/lbers.jpeg';
+import Henri from '../assets/Henri_de_Blainville.jpg';
+import Bolzano from '../assets/Bernard_Bolzano.jpg';
+
 const mockData = [
   {
-    images: '../assets/Blog-Caltech-writers-Bell.jpg',
+    images: Bell,
     body: 'Obvious is the most dangerous word in mathematics.',
     author: 'E. T. Bell',
     id: 1,
   },
   {
-    images: '../assets/lbers.jpeg',
+    images: Bers,
     body: 'Mathematics is a collection of cheap tricks and dirty jokes.',
     author: 'Lipman Bers',
     id: 2,
   },
   {
-    images: '../assets/Henri_de_Blainville.jpg',
+    images: Henri,
     body: 'Life is the twofold internal movement of composition and decomposition at once general and continuous.',
-    author:
-      'Henri de Blainville (American Journal of the Medical Sciences, 1858)',
+    author: 'Henri de Blainville',
     id: 3,
   },
   {
-    images: '../assets/Bernard_Bolzano.jpg',
+    images: Bolzano,
     body: 'My special pleasure in mathematics rested particularly on its purely speculative part.',
-    author: 'Bernhard Bolzano (1781-1848)',
+    author: 'Bernhard Bolzano',
     id: 4,
   },
 ];
