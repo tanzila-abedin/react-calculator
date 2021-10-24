88280d9180eb28beaf4892f1d28b414e397cee8d
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-10-24 21:18:41 +0200

    Test Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,7 @@ import '../style/Home.css';
 
 const Home = () => (
   <div className="home container p-5">
-    <p className="text-center welcome-text">
-      {' '}
+    <p className="text-center welcome-text" data-testid="welcomeText">
       Welcome to Math Magician website
     </p>
   </div>
